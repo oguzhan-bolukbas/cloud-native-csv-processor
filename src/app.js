735ecb8f295b815be00b8d1c4b39b182d1485b57
Oğuzhan Bölükbaş <oguzhan.bolukbas18@gmail.com>
2025-06-18 09:07:19 +0300

    feat(ui): add HTML upload form and display parsed CSV rows in browser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const uploadRoutes = require('./routes/upload');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public'))); // Serve HTML
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/uploads', express.static('src/uploads')); // serve uploaded files
