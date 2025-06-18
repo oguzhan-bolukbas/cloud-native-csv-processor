3ff49c6fc529963c21d5071bda7f15930386b16e
Oğuzhan Bölükbaş <oguzhan.bolukbas18@gmail.com>
2025-06-18 11:37:27 +0300

    feat: The uploaded csv file is uploaded to S3

---
+++ src/utils/s3Uploader.js
@@ -0,0 +1,27 @@
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const s3 = new S3Client({ region: process.env.AWS_REGION });
+
+exports.uploadFileToS3 = async (localPath, originalName) => {
+  const fileStream = fs.createReadStream(localPath);
+  const filename = `${uuidv4()}-${originalName}`;
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: filename,
+    Body: fileStream,
+    ContentType: 'text/csv',
+  };
+
+  const command = new PutObjectCommand(params);
+  await s3.send(command);
+
+  return {
+    Bucket: params.Bucket,
+    Key: params.Key,
+    Location: `https://${params.Bucket}.s3.amazonaws.com/${params.Key}`,
+  };
+};
